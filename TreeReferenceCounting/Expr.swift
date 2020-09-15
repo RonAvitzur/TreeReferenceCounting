c5b065d476218f211866b998f84a467b8638f09e
Ron Avitzur <avitzur@PacificT.com>
2020-09-15 07:01:38 -0700

    Compare recursive function, Unmanaged and Expr iterators

--- TreeReferenceCounting/Expr.swift
+++ TreeReferenceCounting/Expr.swift
@@ -5,35 +5,30 @@
 import Foundation
 
 final class Expr : Sequence {
-    var value: Double = 0
-    weak var parent: Expr? = nil
-    var args: [Expr] = [] { didSet { setArgParents() } }
-    
-    init(_ value: Double, _ args:Expr...) {
-        self.value = value
+    var value: Double
+    weak var parent: Expr?
+    var args: [Expr] { didSet { for arg in args { arg.parent = self } } }
+
+    init(_ args:Expr...) {
+        self.value = 1
         self.args = args
-        setArgParents()
+        for arg in args { arg.parent = self }
     }
     
-    func setArgParents() { for arg in args { arg.parent = self } }
     var numberOfArgs: Int { args.count }
     subscript(index:Int) -> Expr { get { args[index] } }
-    var opnum: Int {
-        guard let parent = parent else { return -1 }
-        return parent.args.firstIndex(where: {self === $0})! // self must be in parent.args. ok to crash if untrue
+    func index(of arg: Expr) -> Int { return args.firstIndex{$0 === arg}! }
+
+    var opnum: Int { parent?.index(of: self) ?? -1 } // self must be in parent.args. ok to crash if untrue
+    var leftMost:  Expr { numberOfArgs == 0 ? self : args[0].leftMost   }
+    var nextBottomUp: Expr? {
+        guard let parent = parent else { return nil }
+        return self === parent[parent.numberOfArgs - 1] ? parent : parent[opnum + 1].leftMost
     }
 
-    var leftMost: Expr { numberOfArgs == 0 ? self : args[0].leftMost }
-    var nextBottomUp: Expr? {
-        guard let parent = parent else      { return nil }
-        if opnum == parent.numberOfArgs - 1 { return parent }
-        else                                { return parent[opnum + 1].leftMost }
-    }
-    
-    func makeIterator() -> ExprBottomUpIterator { ExprBottomUpIterator(self) } // iterate nodes in bottom up order
-    
     /// **for node in expr** iterates in bottom-up left-to-right order starting at the leftmost leaf
-    struct ExprBottomUpIterator : IteratorProtocol {
+    func makeIterator() -> ExprIterator { ExprIterator(self) } // iterate nodes in bottom up order    
+    struct ExprIterator : IteratorProtocol {
         typealias Element = Expr
         
         let top: Expr
@@ -44,74 +39,62 @@ final class Expr : Sequence {
             self.nextNode = top.leftMost
         }
         
-        mutating func next() -> Expr? {
+        mutating
+        func next() -> Expr? {
             let expr = nextNode;
             nextNode = (nextNode === top) ? nil : nextNode?.nextBottomUp
             return expr
         }
     }
-}
-
-
-// another iterator to walk nodes in top down order
-extension Expr {
-    var rightMost: Expr { numberOfArgs == 0 ? self : args[numberOfArgs - 1].rightMost }
-    var nextTopDown: Expr? { numberOfArgs == 0 ? nextOperand : args[0] }
-    var nextOperand: Expr? {
-        var expr = self
-        while true {
-            guard let parent = expr.parent else { return nil }
-            if expr.opnum != parent.numberOfArgs - 1 { return parent[expr.opnum + 1] }
-            else { expr = parent }
-        }
+    
+    // Unmanaged version
+    var opnumUnmanaged: Int {
+        guard let parent = parent else { return -1 }
+        //return Unmanaged.passUnretained(parent)._withUnsafeGuaranteedRef{ $0.args.firstIndex{$0 === self}! } // self must be in parent.args. ok to crash if untrue
+        return Unmanaged.passUnretained(parent)._withUnsafeGuaranteedRef{$0.index(of: self)} // self must be in parent.args. ok to crash if untrue
     }
 
-    struct ExprTopDown: Sequence {
+    var leftMostUnmanaged: Expr { return numberOfArgs == 0 ? self : Unmanaged.passUnretained(args[0])._withUnsafeGuaranteedRef{$0.leftMostUnmanaged} }
+
+    var nextBottomUpUnmanaged: Expr? {
+        guard let parent = parent else { return nil }
+        return Unmanaged.passUnretained(parent)._withUnsafeGuaranteedRef {
+            if self === $0[$0.numberOfArgs - 1] { return $0 }
+            else {
+                return Unmanaged.passUnretained($0[opnumUnmanaged + 1])._withUnsafeGuaranteedRef { $0.leftMostUnmanaged }
+            }
+        }
+    }
+    
+    struct ExprUnmanagedIterator : IteratorProtocol {
+        typealias Element = Expr
+        
+        let top: Unmanaged<Expr>
+        var nextNode: Unmanaged<Expr>?
+        
+        init(_ top: Expr) {
+            self.top = Unmanaged.passUnretained(top)
+            self.nextNode = Unmanaged.passUnretained(top.leftMostUnmanaged)
+        }
+        
+        mutating func next() -> Expr? {
+            guard let nextNode = nextNode else { return nil }
+            return nextNode._withUnsafeGuaranteedRef {
+                expr in self.nextNode = top._withUnsafeGuaranteedRef{$0 === expr} ? nil : Unmanaged.passUnretained(expr.nextBottomUpUnmanaged!)
+                return expr
+            }
+        }
+    }
+    
+    struct UnmanagedSequence: Sequence {
         var expr: Expr
         init(_ expr: Expr) { self.expr = expr }
-        func makeIterator() -> ExprTopDownIterator { return ExprTopDownIterator(expr) }
+        func makeIterator() -> ExprUnmanagedIterator { return ExprUnmanagedIterator(expr) }
     }
 
-    var topDown: ExprTopDown { ExprTopDown(self) }
+    var sumRecursive: Double { value + args.reduce(0){$0 + $1.sumRecursive} }
+    var sumLoop: Double { var count = 0.0; for expr in self { count += expr.value}; return count }
+    var sumUnmanagedLoop: Double { var count = 0.0; for expr in UnmanagedSequence(self) { count += expr.value}; return count }
 }
 
-struct ExprTopDownIterator : IteratorProtocol {
-    typealias Element = Expr
-    
-    let stop: Expr
-    var nextNode: Expr?
-    
-    init(_ top: Expr) {
-        self.stop = top.rightMost
-        self.nextNode = top
-    }
-    
-    mutating func next() -> Expr? {
-        let expr = nextNode;
-        nextNode = (nextNode === stop) ? nil : nextNode?.nextTopDown
-        return expr
-    }
-}
-
-// Is there any way to use Unmanaged<Expr> to iterate without retain/release? This attempt is flawed.
-struct ExprTopDownUnsafeIterator : IteratorProtocol {
-    typealias Element = Expr
-    
-    let stop: Unmanaged<Expr>
-    var nextNode:  Unmanaged<Expr>?
-    
-    init(_ top: Expr) {
-        self.stop = Unmanaged.passUnretained(top.rightMost)
-        self.nextNode = Unmanaged.passUnretained(top)
-    }
-    
-    mutating func next() -> Expr? {
-        guard let nextNode = nextNode else { return nil }
-        return nextNode._withUnsafeGuaranteedRef {
-            let expr = $0;
-            self.nextNode = stop._withUnsafeGuaranteedRef{expr === $0} ? nil : Unmanaged.passUnretained($0.nextTopDown!)
-            return expr
-        }
-    }
-}
 
--- TreeReferenceCounting/main.swift
+++ TreeReferenceCounting/main.swift
@@ -4,52 +4,17 @@
 import Foundation
 
 func makeBigTree(_ depth: Int) -> Expr {
-    return depth == 0 ?
-        Expr(1)
-        : Expr(
-            1,
-            makeBigTree(depth - 1),
-            makeBigTree(depth - 1),
-            makeBigTree(depth - 1),
-            makeBigTree(depth - 1))
-    }
+    return depth == 0 ? Expr() : Expr(makeBigTree(depth - 1), makeBigTree(depth - 1))
+}
 
-func measure(_ title: String, block: () -> ()) {
-    
+func measure(_ title: String = "", block: () -> ()) {
     let startTime = CFAbsoluteTimeGetCurrent()
     block()
     let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
     print(" \(title): " + String(format:"%.2g", timeElapsed)  + " seconds")
 }
 
-
-let expr = makeBigTree(11)
-
-measure("reduce")    { let _ = expr.reduce(0.0){ $0 + $1.value } }
-measure("contains")  { let _ = expr.contains{$0.value == 2} }
-measure("for node in expr") {
-    var count = 0.0
-    for node in expr { count += node.value }
-}
-measure("for node in expr.topDown") {
-    var count = 0.0
-    for node in expr.topDown { count += node.value}
-}
-
-extension Expr {
-    var recursiveTopDownSum: Double {
-        var count = value
-        for arg in args { count += arg.recursiveTopDownSum }
-        return count
-    }
-
-    var recursivBottomUpSum: Double {
-        var count = 0.0
-        for arg in args { count += arg.recursivBottomUpSum }
-        return count + value
-    }
-}
-
-measure("recursiveTopDownSum") { let _ = expr.recursiveTopDownSum }
-measure("recursivBottomUpSum") { let _ = expr.recursivBottomUpSum }
-
+let bigExpr = makeBigTree(22)
+measure("recursive") { print(bigExpr.sumRecursive) }
+measure("loop") { print(bigExpr.sumLoop) }
+measure("unmanaged loop") { print(bigExpr.sumUnmanagedLoop) }
